50d542cfe172d7facb73bf4ba45602089f828030
PriyanshuLathi <priyanshulathi22@gmail.com>
2024-09-03 15:11:05 +0530

    detection-code

--- Python code/Face_detection.py
+++ Python code/Face_detection.py
@@ -2,36 +2,36 @@ import cv2
 import numpy as np
 import time
 
-pengklasifikasiWajah  = cv2.CascadeClassifier("D:\FACE_TRACKING\haarcascade_frontalface_default.xml")
+face_classifier = cv2.CascadeClassifier("D:\FACE_TRACKING\haarcascade_frontalface_default.xml")
 
-videoCam = cv2.VideoCapture(0)
+video_cam = cv2.VideoCapture(0)
 
-if not videoCam.isOpened():
-    print("Kamera tidak dapat diakses")
+if not video_cam.isOpened():
+    print("Camera cannot be accessed")
     exit()
 
-tombolQditekan = False
-while (tombolQditekan == False):
-    ret, kerangka = videoCam.read()
+q_key_pressed = False
+while (q_key_pressed == False):
+    ret, frame = video_cam.read()
 
     if ret == True:
-        abuAbu = cv2.cvtColor(kerangka, cv2.COLOR_BGR2GRAY)
-        dafWajah = pengklasifikasiWajah.detectMultiScale(abuAbu, scaleFactor = 1.3, minNeighbors = 2)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=2)
 
-        for (x, y, w, h) in dafWajah:
-            cv2.rectangle(kerangka, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
-        #print("Jumlah Wajah terdeksi: ", len(dafWajah))
-        teks = "Jumlah Wajah Terdeteksi = " + str(len(dafWajah))
+        # print("Number of faces detected: ", len(faces))
+        text = "Number of Faces Detected = " + str(len(faces))
 
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(kerangka, teks, (0, 30), font, 1, (255, 0, 0), 1)
+        cv2.putText(frame, text, (0, 30), font, 1, (255, 0, 0), 1)
 
-        cv2.imshow("Hasil", kerangka)
+        cv2.imshow("Result", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            tombolQditekan = True
+            q_key_pressed = True
             break
 
 
-videoCam.release()
+video_cam.release()
 cv2.destroyAllWindows()
